b2de3bc393d50ab92e4d5fb68721bb031cf30889
nicoVillagran <nicolasvillagran321@gmail.com>
2026-06-01 12:01:52 -0300

    listas - continuar con ejercicio 9

---
+++ listas/listas-ejercicio9/ejercicio9.pas
@@ -0,0 +1,16 @@
+{ 
+  Utilizando el programa del ejercicio 1, realizar los siguientes módulos:
+    a. EstáOrdenada: recibe la lista como parámetro y retorna true si la misma se encuentra ordenada, o
+    false en caso contrario.
+    b. Eliminar: recibe como parámetros la lista y un valor entero, y elimina dicho valor de la lista (si
+    existe). Nota: la lista podría no estar ordenada.
+    c. Sublista: recibe como parámetros la lista L y dos valores enteros A y B, y retorna una nueva lista
+    con todos los elementos de la lista L mayores que A y menores que B.
+    d. Modifique el módulo Sublista del inciso anterior, suponiendo que la lista L se encuentra ordenada
+    de manera ascendente.
+    e. Modifique el módulo Sublista del inciso anterior, suponiendo que la lista L se encuentra ordenada
+    de manera descendente.
+}
+program ejercicio9;
+begin
+end.
